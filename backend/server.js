fbd44cb13bb9b9ac8da4e6c2c6c4da67364133ba
shaheen bano <bshaheen236@gmail.com>
2023-05-13 02:20:56 +0530

    commit changes

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+
+app.get("/", (req, res) => {
+  res.send("API is runnig");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+app.get("/api/notes/:id",(req,res)=>{
+    const note= notes.find((n)=>n._id==req.params.id);
+    res.send(note); 
+})
+
+
+app.listen(PORT, console.log(`server started in the PORT ${PORT}`));
